2975f0e42bd35b51fd3a913baf37a931dc51704d
Jahaziel Rodriguez <68198368+Young-Prota@users.noreply.github.com>
2020-09-21 16:55:23 -0400

    Created new page
    Fuck divs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,23 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
-    theme: {
-      extend: {
-        colors: {
-          purple: {
-            '100': '#FAF5FF',            
-            '200': '#E9D8FD',
-            '300': '#e0e0e0',
-            '400': '#bdbdbd',
-            '500': '#9e9e9e',
-            '600': '#5E2F74',
-            '700': '#616161',
-            '800': '#424242',
-            '900': '#212121',
-            'grey1' : '#1a202c',
-            'grey2' : '#272c37',
-          }
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          ...colors.purple,
+          'custom-base': '#5E2F74',
+          'custom-darker': '#432252',
         },
-        spacing: {
-          '35' : '9rem',
-          '28' : '7rem',
-          '72': '18rem',
-          '84': '27rem',
-          '96': '29rem',
-        },
-      }
-    }
-  };
+      },
+    },
+    
+  },
+  variants: {},
+  plugins: [],
+};
+
 
 
 
